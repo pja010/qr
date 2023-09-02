183be4e7287ae871f6ce47be163b8d0a110d64e3
pja010 <per.jf.astrom@gmail.com>
2023-09-02 13:31:36 +0200

    works

--- js/cljs-runtime/redirector.js
+++
@@ -1,6 +0,0 @@
-goog.provide('redirector');
-redirector.main = (function redirector$main(){
-return alert("Hello there friend");
-});
-
-//# sourceMappingURL=redirector.js.map
--- js/cljs-runtime/shadow.module.main.append.js
+++
@@ -1,2 +0,0 @@
-
-try { redirector.main(); } catch (e) { console.error("An error occurred when calling (redirector/main)"); throw(e); }
